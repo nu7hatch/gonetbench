f8119333a350e2012cfea80f12fd32baf76d9fa8
Chris Kowalik <dev+nu7hatch@cuboxsa.com>
2012-01-06 15:03:35 -0200

    added pending connections counter, minor fixes

--- main.go
+++ main.go
@@ -20,17 +20,18 @@ var (
 	sc   chan bool // sent packets
 	lc   chan bool // lost packets
 	ac   chan bool // acctive connections
+	pc   chan bool // pending connections
 	ec   chan bool // error connections
 	pack []byte
 )
 
 func init() {
 	flag.UintVar(&Num, "n", 10, "number of concurrent clients")
-	flag.UintVar(&Size, "s", 1, "packet size")
+	flag.UintVar(&Size, "s", 1, "packet size (in bytes)")
 	flag.Parse()
 
-	Addr, Cmd = flag.Arg(0), flag.Arg(1)
-	if Addr == "" || Cmd == "" {
+	Addr = flag.Arg(0)
+	if Addr == "" {
 		usage()
 		os.Exit(1)
 	}
@@ -44,14 +45,18 @@ func usage() {
 func status() {
 	wg.Add(1)
 	defer wg.Done()
-	acs, ecs, scs, lcs := 0, 0, 0, 0
+	acs, ecs, pcs, scs, lcs := 0, 0, 0, 0, 0
 	fmt.Printf("Benchmarking %s with %d concurrent connections:\n\n", Addr, Num)
 	for {
 		select {
 		case <-ac:
 			acs += 1
+			pcs -= 1
 		case <-ec:
 			ecs += 1
+			pcs -= 1
+		case <-pc:
+			pcs += 1
 		case <-sc:
 			scs += 1
 		case <-lc:
@@ -60,7 +65,7 @@ func status() {
 			fmt.Println("")
 			return
 		}
-		fmt.Printf("\rConnections (active: %d, failed: %d), Packets (sent: %d, lost: %d)", acs, ecs, scs, lcs)
+		fmt.Printf("\rConnections (active: %d, pending: %d, failed: %d), Packets (sent: %d, lost: %d)", acs, pcs, ecs, scs, lcs)
 	}
 }
 
@@ -74,14 +79,15 @@ func read(conn net.Conn) {
 func client() {
 	conn, err := net.Dial("tcp", Addr)
 	if err != nil {
+		ec <- true
 		return
 	}
 	ac <- true
 	defer conn.Close()
 	go read(conn)
 	for {
-		n, err := conn.Write([]byte{'x'})
-		if err != nil || n != 1 {
+		n, err := conn.Write(pack)
+		if err != nil || n != len(pack) {
 			lc <- true
 		}
 		sc <- true
@@ -93,6 +99,7 @@ func main() {
 	quit = make(chan bool)
 	ac = make(chan bool)
 	sc = make(chan bool)
+	pc = make(chan bool)
 	ec = make(chan bool)
 	lc = make(chan bool)
 
@@ -103,6 +110,7 @@ func main() {
 	
 	go status()
 	for i := 0; i < int(Num); i += 1 {
+		pc <- true
 		go client()
 	}
 
